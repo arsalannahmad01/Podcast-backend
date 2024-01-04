7121333ed9a297eaf439b95198fd2489a6f51f20
Arsalan Ahmad <arsalannahmad01@gmail.com>
2024-01-04 16:59:35 +0530

    changes api authorization

--- routes/podcast.js
+++ routes/podcast.js
@@ -4,6 +4,5 @@ const router = express.Router()
 const { createPodcast, getPodcast } = require('../controllers/podcast')
 
 router.route('/create-podcast').post(createPodcast)
-router.route('/get-podcast').get(getPodcast)
 
 module.exports = router
--- routes/user.js
+++ routes/user.js
@@ -2,9 +2,12 @@ const express = require('express')
 const router = express.Router()
 
 const { register, login } = require('../controllers/user')
+const {getPodcast} = require('../controllers/podcast')
 
 router.post('/register', register)
 router.post('/login', login)
+router.route('/get-podcast').get(getPodcast)
+
 
 
 module.exports = router
